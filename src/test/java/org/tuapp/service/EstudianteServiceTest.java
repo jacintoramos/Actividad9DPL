20f8e855793408118f2c98c66f6af5f9bf321fb2
Jacinto R. Ramos Moreno <jacinto.ramos@cifpvilladeaguimes.es>
2026-05-08 00:37:24 +0100

    Test EstudianteService modificado

---
+++ src/test/java/org/tuapp/service/EstudianteServiceTest.java
@@ -0,0 +1,60 @@
+package org.tuapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.tuapp.model.Estudiante;
+import org.tuapp.repository.EstudianteRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EstudianteServiceTest {
+
+    @Mock
+    private EstudianteRepository repository;
+
+    @InjectMocks
+    private EstudianteService service;
+
+    private Estudiante estudiante;
+
+    @BeforeEach
+    public void setUp() {
+        estudiante = new Estudiante();
+        estudiante.setId(2L);
+        estudiante.setNombre("Joana Santana");
+        estudiante.setEmail("joana.santana@proton.com");
+        estudiante.setEdad(45);
+    }
+
+    @Test
+    void testGuardarEstudiante() {
+        when(repository.save(any(Estudiante.class))).thenReturn(estudiante);
+
+        Estudiante guardado = service.guardarEstudiante(new Estudiante());
+
+        assertNotNull(guardado);
+        assertEquals("Juan Perez", guardado.getNombre());
+        verify(repository, times(1)).save(any(Estudiante.class));
+
+    }
+
+    @Test
+    void testObtenerTodos() {
+        when(repository.findAll()).thenReturn(List.of(estudiante));
+
+        List<Estudiante> lista = service.obtenerTodos();
+
+        assertFalse(lista.isEmpty());
+        assertEquals(1, lista.size());
+        verify(repository, times(1)).findAll();
+    }
+
+}
