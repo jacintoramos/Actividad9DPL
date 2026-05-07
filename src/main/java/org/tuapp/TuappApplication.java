59c99c00f9a570116dda55c979f7f96a3090fd6a
Jacinto R. Ramos Moreno <jacinto.ramos@cifpvilladeaguimes.es>
2026-05-07 23:27:48 +0100

    Primer Commit

---
+++ src/main/java/org/tuapp/TuappApplication.java
@@ -0,0 +1,13 @@
+package org.tuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tuapp/TuappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
