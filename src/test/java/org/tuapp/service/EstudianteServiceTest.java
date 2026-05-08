f7c7b3d55ab14f342b53f3615d8d10dd89266bd6
Jacinto R. Ramos Moreno <jacinto.ramos@cifpvilladeaguimes.es>
2026-05-08 12:17:57 +0100

    Test EstudianteService modificado 5

--- src/test/java/org/tuapp/service/EstudianteServiceTest.java
+++ src/test/java/org/tuapp/service/EstudianteServiceTest.java
@@ -41,7 +41,7 @@ public class EstudianteServiceTest {
         Estudiante guardado = service.guardarEstudiante(new Estudiante());
 
         assertNotNull(guardado);
-        assertEquals("Bencomo Alonso", guardado.getNombre());
+        assertEquals("Benito Cabrera", guardado.getNombre());
         verify(repository, times(1)).save(any(Estudiante.class));
 
     }
